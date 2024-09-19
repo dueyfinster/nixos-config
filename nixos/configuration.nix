f9cfdeda772d58eb833274fdbdb8095cfbca21e6
Neil Grogan <neil@grogan.org>
2024-09-20 00:11:59 +0100

    Add flakes and nix-cmd support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,6 +26,8 @@
     XDG_CONFIG_HOME = "\${HOME}/.config";
     XCURSOR_SIZE = "24";
   };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
   environment.systemPackages = with pkgs; [
     vim
