1211b4757f53058566c330011024464a539a71cd
Neil Grogan <neil@grogan.org>
2024-09-22 21:20:27 +0100

    added zsh completion

--- home-manager/config/zsh.nix
+++ home-manager/config/zsh.nix
@@ -1,4 +1,10 @@
 {config, pkgs, ...}:
 {
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    # enable completion
+    enableCompletion = true;
+    completionInit = "autoload -U compinit && compinit -i";
+    autosuggestion.enable = true;
+  };
 }
