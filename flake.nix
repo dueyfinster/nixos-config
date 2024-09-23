2bc963472c0fa96e504e0e8807a61570f2648b60
Neil Grogan <neil@grogan.org>
2024-09-23 23:38:16 +0100

    added nix-darwin as flake input

--- flake.nix
+++ flake.nix
@@ -15,6 +15,9 @@
 
     cachix.url = "github:cachix/cachix";
     devenv.url = "github:cachix/devenv";
+    
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
