aae79a5e30c39ae82ab81de044f5a2c7078e3781
Neil Grogan <neil@grogan.org>
2024-09-24 17:33:02 +0100

    try link sway conf via nix

--- hosts/compage/home.nix
+++ hosts/compage/home.nix
@@ -121,6 +121,19 @@
 
 	nixpkgs.config.allowUnfree = true;
 
+	home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    ".config/sway/config".source = ../../modules/nixos/wayland/sway/config;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
   home.username = "ngrogan";
   home.homeDirectory = "/home/ngrogan";
   home.stateVersion = "24.05";
