9ad880713a079d50419b75cbc076f4bc3e46e170
Neil Grogan <neil@grogan.org>
2024-10-01 20:44:32 +0100

    Added btop as option

---
+++ modules/home-manager/btop.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  options = {
+    programs.btop.enable = lib.mkOption {
+      description = "Whether to install btop, check cpu, disk, ram etc.";
+      type = lib.types.bool;
+      default = false;
+    };
+  };
+  config = lib.mkIf config.programs.btop.enable {
+   programs.btop = {
+    enable = true;
+    settings = {
+      vim_keys = true;
+     };
+    };
+  };
+}
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,6 +2,7 @@
 
 {
     imports = [
+      ./btop.nix
       ./thonny.nix
       ./wine.nix
     ];
